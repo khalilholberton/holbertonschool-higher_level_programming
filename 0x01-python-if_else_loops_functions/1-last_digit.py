de16b6f2fd5c321cee6339dde4d73842c4a9f35f
khalilholberton <1071@holbertonschool.com>
2019-12-10 19:40:29 +0000

    fix

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,13 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 if number >= 0:
-    num = number % 10
+    c = number % 10
 str = "and is less than 6 and not 0"
 if number < 0:
-    num = number % (10 * (-1))
-if num == 0:
-    print("Last digit of {} is {} and is 0".format(number, num))
-elif num > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, num))
-elif num < 6 and num != 0:
-    print("Last digit of {} is {} and {}".format(number, num, str))
+    c = number % (10 * (-1))
+if c == 0:
+    print("Last digit of {:d} is {:d} and is 0".format(number, c))
+elif c > 5:
+    print("Last digit of {:d} is {:d} and is greater than 5".format(number, c))
+elif c < 6 and c != 0:
+    print("Last digit of {:d} is {:d} {:s}".format(number, c, str))
