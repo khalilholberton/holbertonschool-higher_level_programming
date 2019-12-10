4829cf31119f5751e0a29adab4b7cfaaddb63c01
khalilholberton <1071@holbertonschool.com>
2019-12-10 22:52:36 +0000

    fix

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for c in range(0, 99):
-    if c <= 9:
-        print(0, end="")
-    print("{}".format(c), end=", " if c < 98 else ', {}\n'.format(99))
+    print("{:02d}, ".format(c), end="")
+print("99")
