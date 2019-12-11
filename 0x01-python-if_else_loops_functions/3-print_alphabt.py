bed6200b5d7434b856da278540b015771a7b269e
khalilholberton <1071@holbertonschool.com>
2019-12-11 08:10:55 +0000

    fix

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for c in range(97, 123):
-    if c != 101 and c != 113:
-        print("{}".format(chr(i)), end='')
+for c in 'abcdefghijklmnopqrstuvwxyz':
+    if (c != "q" and c != "e"):
+        print("{}".format(c), end="")
