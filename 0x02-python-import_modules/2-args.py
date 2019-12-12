eb9f03e7c028df226eb610a7ee6b27d20361fdf4
khalilholberton <1071@holbertonschool.com>
2019-12-12 16:23:38 +0000

    fix

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) <= 1:
+        print("0 argument.")
+    if (len(sys.argv)) == 2:
+            print("{:d} argument:".format((len(sys.argv)) - 1))
+    else:
+        print("{:d} arguments:".format((len(sys.argv)) - 1))
+    for x in range(1, (len(sys.argv))):
+        print("{:d}: {}".format(x, sys.argv[x]))
