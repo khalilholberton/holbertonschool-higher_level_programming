76b6b04df76e20a049bc7d3647d3366d7298dec2
khalilholberton <1071@holbertonschool.com>
2020-01-08 16:35:09 +0000

    pep8 fixed

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     num = 0
     try:
-        for element in range(0,x):
+        for element in range(0, x):
             print("{}".format(my_list[element]), end="")
             num += 1
     except IndexError:
