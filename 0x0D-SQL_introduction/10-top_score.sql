c91cf07888e93a815a16b992bef115d57cc9c315
khalilholberton <1071@holbertonschool.com>
2020-03-02 21:19:13 +0000

    fix

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,2 @@
+-- MySql script that lists all records of the table second_table 
+SELECT score, name FROM second_table ORDER by score DESC;
---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,2 @@
+-- MySql Script that inserts a new row in the table first_table
+INSERT INTO  first_table (id, name) VALUES (89, "Holberton School");
