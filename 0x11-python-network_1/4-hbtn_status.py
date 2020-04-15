4a5fb90f1b237465f2e5ece56d6dcf150d816627
khalilholberton <1071@holbertonschool.com>
2020-04-15 22:28:39 +0000

    fix

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Python script that fetches url
+
+import requests
+
+if __name__ == "__main__":
+    r = requests.get("https://intranet.hbtn.io/status").text
+    print("Body response:")
+    print("\t type {}".format(type(r)))
+    print("\t content: {}".format(r))
