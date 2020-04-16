b0cb649bbcf94d42f7d4c61f4e90532c7a44fe04
khalilholberton <1071@holbertonschool.com>
2020-04-16 19:41:35 +0000

    fix

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,7 +7,8 @@ import requests
 from sys import argv
 if __name__ == "__main__":
     try:
-        r = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
+        r = requests.get('https://api.github.com/user',
+                         auth=(argv[1], argv[2]))
         print(r.json().get('id'))
     except KeyError:
         print('None')
