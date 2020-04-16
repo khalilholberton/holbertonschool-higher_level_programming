d2b5faf9204ffd67679ff50f1aa70feb4e5f2c40
khalilholberton <1071@holbertonschool.com>
2020-04-16 19:26:36 +0000

    fix

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,7 +7,7 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    q = argv[1] if len(argv) > 1 else ""
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
     try:
         r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
         r = r.json()
