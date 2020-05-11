84b296a3432a5f248c9cbe0e5e856d303e3a24c9
khalilholberton <1071@holbertonschool.com>
2020-05-11 14:15:42 +0000

    fix

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let idx = 0
+    for (idx = 0; idx < this.height; idx++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    let container = this.width;
+    this.width = this.height;
+    this.height = container;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+module.exports = Rectangle;
