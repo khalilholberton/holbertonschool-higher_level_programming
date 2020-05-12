617c6ad806a1d614af7e8185fcc6caa1f7928013
khalilholberton <1071@holbertonschool.com>
2020-05-12 22:13:38 +0000

    fix

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,12 +2,9 @@
 
 const request = require('request');
 
-const epID = process.argv[2];
+const ID = process.argv[2];
 
-request('http://swapi.co/api/films/' + epID, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(JSON.parse(body).title);
-  }
+request.get('https://swapi-api.hbtn.io/api/films/' + ID, function (err, response, body) {
+  if (err) throw err;
+  console.log(JSON.parse(body).title);
 });
